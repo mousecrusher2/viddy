af1e78be495814576f111b44a4b35372f6856ba8
mousecrusher2 <95418154+mousecrusher2@users.noreply.github.com>
2026-07-01 14:10:21 +0900

    refactor: update environment variable handling for logging and data directories

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 use vergen_gix::{Build, Emitter, Gix};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let env_prefix = std::env::var("CARGO_PKG_NAME")?.to_ascii_uppercase();
+    println!("cargo:rustc-env=PACKAGE_ENV_PREFIX={env_prefix}");
+
     Emitter::default()
         .default_on_error()
         .add_instructions(&Build::all_build())?
--- src/utils.rs
+++ src/utils.rs
@@ -10,18 +10,14 @@ use tracing_subscriber::{
     util::SubscriberInitExt,
 };
 
-static PROJECT_NAME: LazyLock<String> = LazyLock::new(|| env!("CARGO_CRATE_NAME").to_uppercase());
-static DATA_FOLDER: LazyLock<Option<PathBuf>> = LazyLock::new(|| {
-    std::env::var(format!("{}_DATA", *PROJECT_NAME))
-        .ok()
-        .map(PathBuf::from)
-});
-static CONFIG_FOLDER: LazyLock<Option<PathBuf>> = LazyLock::new(|| {
-    std::env::var(format!("{}_CONFIG", *PROJECT_NAME))
-        .ok()
-        .map(PathBuf::from)
-});
-static LOG_ENV: LazyLock<String> = LazyLock::new(|| format!("{}_LOGLEVEL", *PROJECT_NAME));
+const DATA_ENV: &str = concat!(env!("PACKAGE_ENV_PREFIX"), "_DATA");
+const CONFIG_ENV: &str = concat!(env!("PACKAGE_ENV_PREFIX"), "_CONFIG");
+const LOG_ENV: &str = concat!(env!("PACKAGE_ENV_PREFIX"), "_LOGLEVEL");
+
+static DATA_FOLDER: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| std::env::var(DATA_ENV).ok().map(PathBuf::from));
+static CONFIG_FOLDER: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| std::env::var(CONFIG_ENV).ok().map(PathBuf::from));
 const LOG_FILE: &str = concat!(env!("CARGO_PKG_NAME"), ".log");
 static PROJECT_DIRECTORY: LazyLock<Option<ProjectDirs>> =
     LazyLock::new(|| ProjectDirs::from("dev", "sachaos", env!("CARGO_PKG_NAME")));
@@ -60,11 +56,11 @@ pub fn get_config_dir() -> PathBuf {
 
 fn logging_filter(env_var: impl Fn(&str) -> Option<String>) -> EnvFilter {
     let rust_log = env_var(EnvFilter::DEFAULT_ENV);
-    let viddy_log_level = env_var(LOG_ENV.as_str());
+    let package_log_level = env_var(LOG_ENV);
     let default_filter = format!("{}=info", env!("CARGO_CRATE_NAME"));
     let log_filter = rust_log
         .as_deref()
-        .or(viddy_log_level.as_deref())
+        .or(package_log_level.as_deref())
         .unwrap_or(default_filter.as_str());
 
     EnvFilter::builder()
@@ -114,14 +110,14 @@ mod tests {
     const ENV_CASES: &[EnvCase] = &[
         EnvCase {
             value: None,
-            selected_filter: "viddy=info",
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=info"),
         },
         EnvCase {
             value: Some(""),
             selected_filter: "error",
         },
         EnvCase {
-            value: Some("viddy=bogus"),
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=bogus")),
             selected_filter: "error",
         },
         EnvCase {
@@ -149,28 +145,28 @@ mod tests {
             selected_filter: "trace",
         },
         EnvCase {
-            value: Some("viddy=off"),
-            selected_filter: "viddy=off",
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=off")),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=off"),
         },
         EnvCase {
-            value: Some("viddy=error"),
-            selected_filter: "viddy=error",
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=error")),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=error"),
         },
         EnvCase {
-            value: Some("viddy=warn"),
-            selected_filter: "viddy=warn",
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=warn")),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=warn"),
         },
         EnvCase {
-            value: Some("viddy=info"),
-            selected_filter: "viddy=info",
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=info")),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=info"),
         },
         EnvCase {
-            value: Some("viddy=debug"),
-            selected_filter: "viddy=debug",
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=debug")),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=debug"),
         },
         EnvCase {
-            value: Some("viddy=trace"),
-            selected_filter: "viddy=trace",
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=trace")),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=trace"),
         },
         EnvCase {
             value: Some("other_crate=off"),
@@ -197,15 +193,23 @@ mod tests {
             selected_filter: "other_crate=trace",
         },
         EnvCase {
-            value: Some("viddy=bogus,debug"),
+            value: Some(concat!(env!("CARGO_CRATE_NAME"), "=bogus,debug")),
             selected_filter: "debug",
         },
         EnvCase {
-            value: Some("viddy=bogus,viddy=debug"),
-            selected_filter: "viddy=debug",
+            value: Some(concat!(
+                env!("CARGO_CRATE_NAME"),
+                "=bogus,",
+                env!("CARGO_CRATE_NAME"),
+                "=debug"
+            )),
+            selected_filter: concat!(env!("CARGO_CRATE_NAME"), "=debug"),
         },
         EnvCase {
-            value: Some("viddy=bogus,other_crate=debug"),
+            value: Some(concat!(
+                env!("CARGO_CRATE_NAME"),
+                "=bogus,other_crate=debug"
+            )),
             selected_filter: "other_crate=debug",
         },
     ];
@@ -213,44 +217,45 @@ mod tests {
     #[test]
     fn logging_filter_preserves_rust_log_fallback_behavior_for_all_env_states() {
         for rust_log in ENV_CASES {
-            for viddy_log_level in ENV_CASES {
+            for package_log_level in ENV_CASES {
                 let read_rust_log = Cell::new(false);
-                let read_viddy_log_level = Cell::new(false);
+                let read_package_log_level = Cell::new(false);
 
                 let filter = logging_filter(|key| match key {
                     EnvFilter::DEFAULT_ENV => {
                         read_rust_log.set(true);
                         rust_log.value.map(ToString::to_string)
                     }
-                    key if key == LOG_ENV.as_str() => {
-                        read_viddy_log_level.set(true);
-                        viddy_log_level.value.map(ToString::to_string)
+                    key if key == LOG_ENV => {
+                        read_package_log_level.set(true);
+                        package_log_level.value.map(ToString::to_string)
                     }
                     _ => unreachable!("unexpected env var key: {key}"),
                 });
-                let expected_filter =
-                    rust_log.value.map_or(viddy_log_level.selected_filter, |_| {
+                let expected_filter = rust_log
+                    .value
+                    .map_or(package_log_level.selected_filter, |_| {
                         rust_log.selected_filter
                     });
 
                 assert_eq!(
                     filter.to_string(),
                     expected_filter,
-                    "RUST_LOG={:?}, VIDDY_LOGLEVEL={:?}",
+                    "RUST_LOG={:?}, {LOG_ENV}={:?}",
                     rust_log.value,
-                    viddy_log_level.value,
+                    package_log_level.value,
                 );
                 assert!(
                     read_rust_log.get(),
-                    "RUST_LOG was not read for RUST_LOG={:?}, VIDDY_LOGLEVEL={:?}",
+                    "RUST_LOG was not read for RUST_LOG={:?}, {LOG_ENV}={:?}",
                     rust_log.value,
-                    viddy_log_level.value,
+                    package_log_level.value,
                 );
                 assert!(
-                    read_viddy_log_level.get(),
-                    "VIDDY_LOGLEVEL was not read for RUST_LOG={:?}, VIDDY_LOGLEVEL={:?}",
+                    read_package_log_level.get(),
+                    "{LOG_ENV} was not read for RUST_LOG={:?}, {LOG_ENV}={:?}",
                     rust_log.value,
-                    viddy_log_level.value,
+                    package_log_level.value,
                 );
             }
         }
